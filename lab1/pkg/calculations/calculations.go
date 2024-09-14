7b1affe84ee79a002653ca1582a3bb760be3426d
Georgii Safonkin <georgiisafonk@gmail.com>
2024-09-14 13:52:58 +0700

    getDiscriminant func added

--- lab1/pkg/calculations/calculations.go
+++ lab1/pkg/calculations/calculations.go
@@ -1,5 +1,9 @@
 package calculations
 
+import (
+	"math"
+)
+
 type Calculator struct {
 	a float32
 	b float32
@@ -8,3 +12,23 @@ type Calculator struct {
 	delta float32
 }
 
+func (c Calculator) getExtremumPoints() []float32 {
+	switch d := getDiscriminant(c.a, c.b) {
+	case d < 0:
+	case d == 0:
+	case d > 0:
+	}
+}
+
+func getDiscriminant(a, b float32) float32 {
+	var new_a, new_b, new_c float32
+	var discriminant float32
+
+	new_a = 3
+	new_b = 2 * a
+	new_c = b
+	
+	discriminant = math.Pow(new_b, 2) - 4 * new_a * new_c
+	
+	return discriminant
+}
