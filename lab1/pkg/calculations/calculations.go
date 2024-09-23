f8d3652e00aed3889e5cc115557eb46c7b66d681
Georgii Safonkin <georgiisafonk@gmail.com>
2024-09-23 11:23:50 +0700

    getDerivative updated

--- lab1/pkg/calculations/calculations.go
+++ lab1/pkg/calculations/calculations.go
@@ -39,7 +39,34 @@ func getDiscriminant(a, b, c float64) float64 {
 	return discriminant
 }
 
+//if order == 3, coefficients len is 4
+//if order == 2, coefficients len is 3
+//etc.
 func getDerivative(order int32, coefficients []float64) []float64 {
-	
+	switch order {
+	case 3:
+		rv := [3]float64 //0 - a, 1 - b, 2 - c
+		rv[0] = 3 * coefficients[0]
+		rv[1] = 2 * coefficients[1]
+		rv[2] = coefficients[2]
+		return rv
+	case 2:
+		rv := [2]float64
+		rv[0] = 2 * coefficients[0]
+		rv[1] = coefficients[1]
+		return rv
+	case 1:
+		rv := [1]float64
+		rv[0] = coefficients[0]
+		return rv
+	}
 	return nil
+}
+
+func moveRight() {
+
+}
+
+func moveLeft() {
+	
 }
